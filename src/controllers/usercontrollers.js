2a704a8637da8ae42ab356e5d7505fac197dc2ee
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-25 14:00:45 -0300

    login func

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -4,45 +4,23 @@ import { pool } from "../db.js";
 //user and password validation
 
 
-export const inguser = function(request, response, next){
+export const inguser = function(request, response){
 
     var username = request.body.username;
-
-    var password = request.body.password;
-
-    if(username && password)
-    {
-        query = `
-        SELECT * FROM users 
-        WHERE username = "${username}"
-        `;
-
-        pool.query(query, function(error, data){
-
-            if(data.length > 0)
-            {
-                for(var count = 0; count < data.length; count++)
-                {
-                    if(data[count].password == password)
-                    {
-                        
-
-                        ver = true;
-                    }
-                    else
-                    {
-                        ver = false;
-                    }
-                }
-            }
-            else
-            {
-                ver = false;
-            }
-            response.end();
-        });
-
-    };
+	var password = request.body.password;
+	if (username && password) {
+		pool.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+               ver = true;
+			} else {
+				ver = false;
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
 };
 
     
