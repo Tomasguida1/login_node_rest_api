515e94272984615fa830f7eb87f47c8527be9ab9
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-25 14:22:13 -0300

    ,

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -4,10 +4,10 @@ import { pool } from "../db.js";
 //user and password validation
 
 
-export const inguser = (req, res) =>{
+export const inguser = async(req, res) =>{
 
     const {username, password} = req.body 
-	usern = pool.query('SELECT username FROM users') ;
+	usern = await pool.query('SELECT username FROM users') ;
 			if (username === usern) {
                 pass = pool.query('SELECT password FROM users')
                 if (password=== pass) {
