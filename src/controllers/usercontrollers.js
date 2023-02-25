f22ccceea2ef42d26c4e4d32eb80c2c596d2d2d0
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-25 13:56:20 -0300

    added login function

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -4,19 +4,46 @@ import { pool } from "../db.js";
 //user and password validation
 
 
-export const inguser = async(req, res) => {
-    const {username, password} = req.body
+export const inguser = function(request, response, next){
 
-    const [rows] = await pool.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password] )
-    if(rows.length > 0) {
-        ver = true;
-      } else {
-        ver = false;
-    }}
-    ;
-  
+    var username = request.body.username;
 
+    var password = request.body.password;
 
+    if(username && password)
+    {
+        query = `
+        SELECT * FROM users 
+        WHERE username = "${username}"
+        `;
+
+        pool.query(query, function(error, data){
+
+            if(data.length > 0)
+            {
+                for(var count = 0; count < data.length; count++)
+                {
+                    if(data[count].password == password)
+                    {
+                        
+
+                        ver = true;
+                    }
+                    else
+                    {
+                        ver = false;
+                    }
+                }
+            }
+            else
+            {
+                ver = false;
+            }
+            response.end();
+        });
+
+    };
+};
 
     
 
