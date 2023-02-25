b9f2c4046b696334b57c674644d6858e68813f8c
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-25 15:54:01 -0300

    try4

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -4,18 +4,23 @@ import { pool } from "../db.js";
 //user and password validation
 
 
-export const inguser =(req, res) => {
-	let username = req.body.username;
-	let password = req.body.password;
-
-    pool.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, rows) => {
-      if(rows.length > 0) {
-        ver = true;
-      } else {
-        ver = false;
-      }
-    });
-  }
+export const inguser =async (req, res) => {
+try{
+    const { username, password } = req.body;
+    if (username && password) {
+        const [rows] = await pool.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], async (err, rows) => {
+        if (rows.length == 0){
+            ver = false;
+        }else{
+            ver = true;
+        }
+     }
+    )
+}
+}catch(error){
+    return res.status(500).json({message: "error"}) 
+}
+}
     
 
 
