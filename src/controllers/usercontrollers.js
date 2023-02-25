1c1339ad50c87949f194306ac74c0bb11992c7e7
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-25 14:26:12 -0300

    catch

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -5,12 +5,12 @@ import { pool } from "../db.js";
 
 
 export const inguser = async(req, res) =>{
-
+try {
     const {username, password} = req.body 
 	usern = await pool.query('SELECT username FROM users') ;
 			if (username === usern) {
                 pass = pool.query('SELECT password FROM users')
-                if (password=== pass) {
+                if (password === pass) {
                 ver = true;
                 } else {
                     ver = false;
@@ -19,8 +19,9 @@ export const inguser = async(req, res) =>{
 				    ver = false;
 			}			
 
-		};
-
+		}catch (error) {
+        return res.status(500).json({message: "error"})    
+    }
 
     
 
