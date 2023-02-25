6ad20b330c8fdbcbbc0a3b354c6d07de694514ad
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-25 12:35:46 -0300

    test

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -4,17 +4,18 @@ import { pool } from "../db.js";
 //user and password validation
 
 
-export const inguser = function(request, response)  {
-    let username = request.body.username;
-	let password = request.body.password;
-    if (username && password) {
-    pool.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {       
-        if (error) 
-        ver = false;
-        if (results.length > 0) {
-            ver = true;
-        }
-    })}};
+export const inguser = async (req, res) => {
+    const { username, password } = req.body;
+    const users = await getUser(username);
+    if (!users) {
+      ver = false;
+    }
+    const passwordMatch = await bcrypt.compare(password, users.password);
+    if (!passwordMatch) {
+      ver = false;;
+    }
+
+  };
 
 
     
