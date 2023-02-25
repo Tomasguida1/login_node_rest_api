c819dfa3295499dd62e80315faa328bf46e43d9c
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-25 13:42:04 -0300

    ¨

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -7,14 +7,14 @@ import { pool } from "../db.js";
 export const inguser = async(req, res) => {
     const {username, password} = req.body
 
-    const [rows] = await pool.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, rows) => {
-      if(rows.length > 0) {
+    const [rows] = await pool.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password] )
+    if(rows.length > 0) {
         ver = true;
       } else {
         ver = false;
-      }
-    });
-  }
+    }}
+    ;
+  
 
 
 
