8c529adc6e2245463d320596869fbf5f24f3b1c0
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-27 12:36:23 -0300

    catch error

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -4,11 +4,11 @@ import { pool } from "../db.js";
 //user and password validation
 
 
-export const inguser = (req, res) => {
+export const inguser = async(req, res) => {
     const {username, password} = req.body;
 try{
         
-        const {result} =  pool.query("SELECT * FROM users WHERE username = ?, password = ?",[username, password] ) ;
+        const {result} =  await pool.query("SELECT * FROM users WHERE username = ?, password = ?",[username, password] ) ;
         if(result.length > 0){
             ver = true;
         } else {
@@ -18,7 +18,7 @@ try{
         res.json(ver);
 
 }catch(error){
-    return res.status(500).json({message: "error"}) 
+    return res.status(500).json({message: error}) 
 }
 }
     
