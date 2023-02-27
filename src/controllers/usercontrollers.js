6052cecdcea535c32db40a94fa4a1299d63822c7
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-27 15:48:09 -0300

    promise test

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -4,12 +4,12 @@ import { pool } from "../db.js";
 //user and password validation
 
 
-export const inguser = (req, res) => {
+export const inguser = async(req, res) => {
     const {username} = req.body
     const {password} = req.body
     try {
         console.log("Buscando usuario con el nombre de usuario y la contraseña:", username, password);
-        const { rows } =  pool.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password]);
+        const { rows } = await pool.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password]);
         console.log("Resultado de la consulta SQL:", rows);
 
         if  (rows.length <= 0) {
