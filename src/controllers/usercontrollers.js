32e81f5c6d69189ab9b7b655d2460d0504ec17ae
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-27 14:36:20 -0300

    query fix8

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -6,23 +6,26 @@ import { pool } from "../db.js";
 
 export const inguser = async(req, res) => {
     const {username,password} = req.body
-try{
-        
-        const {result} =  await pool.query("SELECT * FROM users WHERE  username = ${[username]},  AND password = ${[password]} " ) ;
-        if(result.length > 0){
-            ver = true;
+    try {
+        const { result } = await pool.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password]);
+      
+        if (result.length > 0) {
+          ver = true;
         } else {
           ver = false;
         }
+      
+        return res.json(ver);
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message });
+      }
 
-        res.json(ver);
-
-
-}catch(error){
-    return res.status(500).json({message: error}) 
-}
-}
-    
+      
+      
+      
+      
+      
 
 
 
